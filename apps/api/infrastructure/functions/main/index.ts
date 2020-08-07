783c0d0cc3cfc196821ca83b80cfe877321bb16c
Ying Wang <yingw787.personal@gmail.com>
2020-08-07 18:19:21 -0400

    Revert "Add / update misc files runcmd 'npx nx add @dev-thought/nx-deploy-it', select 'gcp', select both apps to be deployed, select us-east4 as region, and select www.example.com to be the custom URL."
    This reverts commit 0903b129f4fbedd624997da7e50a5d671de0b09c.

--- apps/api/infrastructure/functions/main/index.ts
+++
@@ -1,7 +0,0 @@
-import { createNestServer } from '../../../src/main.gcp';
-import * as express from 'express';
-
-const expressApp = express();
-createNestServer(expressApp);
-
-export const handler = expressApp;
